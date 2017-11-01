1fba8e154fe86c8dc0d84a90cc32890cec73f3ce
qiaoqioayuxia <1937189775@qq.com>
2017-11-01 12:16:47 +0800

    shyavgd

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+ <div>{{msg}} {{age}}</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      age:18
+    }
+  }
+}
+</script>
+<style>
+div{background:red}
+
+</style>
